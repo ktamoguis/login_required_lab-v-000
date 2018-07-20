8634882b852b0411f1347d7b71abc4dd41a650b3
Karl Amoguis <ktamoguis@gmail.com>
2018-07-20 00:54:25 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       binding.pry
       session[:name] = params[:name]
       binding.pry
-      redirect_to 
+      redirect_to secrets_show_path
     end
   end
 
