f676e78875bf26fd5ea6aad7dc90caf35476bba9
Karl Amoguis <ktamoguis@gmail.com>
2018-07-20 00:36:13 +0000

    automatically backed up by learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,7 +2,7 @@ class SecretsController < ApplicationController
 
   def show
     binding.pry
-    if session.empty?
+    if !session.empty?
     else
       binding.pry
       redirect_to login_path
